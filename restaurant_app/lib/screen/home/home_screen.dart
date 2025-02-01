17dfd970eac92ff6de26f1cd5c2883ba672eb6ff
Setyo Adi Sasono <setyoadisasono@gmail.com>
2025-02-01 10:45:48 +0700

    add home screen in restaurant app

---
+++ restaurant_app/lib/screen/home/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_app/model/restaurant.dart';
+import 'package:restaurant_app/screen/home/restaurant_card_widget.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Restaurant Home'),
+      ),
+      body: ListView.builder(
+        itemCount: restaurantList.length,
+        itemBuilder: (context, index) {
+          final restaurant = restaurantList[index];
+
+          return RestaurantCardWidget(
+            restaurant: restaurant,
+          );
+        },
+      ),
+    );
+  }
+}
